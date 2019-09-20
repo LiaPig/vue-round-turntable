43835b56ea549cce9e0ff26658a59116c11e944c
LiaPig <418607528@qq.com>
2019-09-20 10:52:31 +0800

    奖品配置从转盘组件里写样式独立到可在调用转盘组件时的父组件里定义

--- src/App.vue
+++ src/App.vue
@@ -1,116 +1,123 @@
 <template>
   <div id="app">
     <h1>剩余抽奖次数：{{ num }}</h1>
-    <roundTurntable
+    <round-turntable
       ref="roundTurntable"
       :prizeData="prizeData"
       :rotateCircle="rotateCircle"
       :turntableStyleOption="turntableStyleOption"
       @endRotation="endRotation"
-      class="turntable"/>
+      class="turntable">
+      <template slot="item" slot-scope="scope">
+        <div class="turntable-name">{{ scope.item.prizeName }}</div>
+        <div class="turntable-img">
+          <img :src="scope.item.prizeImg">
+        </div>
+      </template>
+    </round-turntable>
     <div @click="startRotation" class="pointer"></div>
   </div>
 </template>
 
 <script>
-    import roundTurntable from './components/roundTurntable'
+  import roundTurntable from './components/roundTurntable';
 
-    export default {
-        name: 'App',
-        components: {
-            roundTurntable
+  export default {
+    name: 'App',
+    components: {
+      roundTurntable
+    },
+    data() {
+      return {
+        // 转盘上的奖品数据
+        prizeData: [
+          {
+            id: 1,
+            prizeName: '2000元京东券',
+            prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/1.png',
+          },
+          {
+            id: 2,
+            prizeName: '300元京东券',
+            prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/7.png',
+          },
+          {
+            id: 3,
+            prizeName: '50个比特币',
+            prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/3.png',
+          },
+          {
+            id: 4,
+            prizeName: '50元话费券',
+            prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/4.png',
+          },
+          {
+            id: 5,
+            prizeName: '100元话费券',
+            prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/5.png',
+          },
+          {
+            id: 6,
+            prizeName: '100个比特币',
+            prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/6.png',
+          }
+        ],
+        // 转动的圈数
+        rotateCircle: 6,
+        // 转盘样式的选项
+        turntableStyleOption: {
+          // 背景色
+          prizeBgColors: ['#AE3EFF', '#4D3FFF', '#FC262C', '#3A8BFF', '#EE7602', '#FE339F'],
+          // 转盘的外边框颜色
+          borderColor: '#199301',
         },
-        data() {
-            return {
-                // 转盘上的奖品数据
-                prizeData: [
-                    {
-                        id: 1,
-                        prizeName: '2000元京东券',
-                        prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/1.png',
-                    },
-                    {
-                        id: 2,
-                        prizeName: '300元京东券',
-                        prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/7.png',
-                    },
-                    {
-                        id: 3,
-                        prizeName: '50个比特币',
-                        prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/3.png',
-                    },
-                    {
-                        id: 4,
-                        prizeName: '50元话费券',
-                        prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/4.png',
-                    },
-                    {
-                        id: 5,
-                        prizeName: '100元话费券',
-                        prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/5.png',
-                    },
-                    {
-                        id: 6,
-                        prizeName: '100个比特币',
-                        prizeImg: 'http://demo.sc.chinaz.net/Files/DownLoad/webjs1/201803/jiaoben5789/images/6.png',
-                    }
-                ],
-                // 转动的圈数
-                rotateCircle: 6,
-                // 转盘样式的选项
-                turntableStyleOption: {
-                    // 背景色
-                    prizeBgColors: ['#AE3EFF', '#4D3FFF', '#FC262C', '#3A8BFF', '#EE7602', '#FE339F'],
-                    // 转盘的外边框颜色
-                    borderColor: '#199301',
-                },
 
-                // 中奖的奖品的index
-                prizeIndex: -1,
-                // 用来锁定转盘，避免同时多次点击转动
-                isLocking: false,
+        // 中奖的奖品的index
+        prizeIndex: -1,
+        // 用来锁定转盘，避免同时多次点击转动
+        isLocking: false,
 
-                // 剩余抽奖次数
-                num: 2,
-            }
-        },
-        methods: {
-            startRotation() {
-                // 如果还不可以转动
-                if (!this.canBeRotated()) {
-                    return false;
-                }
-                // 开始转动
-                // 先上锁
-                this.isLocking = true;
-                // 设置在哪里停下，应该与后台交互，这里随机抽取0~5
-                const index = Math.floor(Math.random() * this.prizeData.length);
-                // 成功后次数减少一次
-                this.num--;
-                this.prizeIndex = index;
-                // 告诉子组件，开始转动了
-                this.$refs.roundTurntable.rotate(index);
-            },
-            // 已经转动完转盘触发的函数
-            endRotation() {
-                // 提示中奖
-                alert(`恭喜您获奖啦,您的奖品是：${this.prizeData[this.prizeIndex].prizeName}`);
-                // 解锁
-                this.isLocking = false;
-            },
-            // 判断是否可以转动
-            canBeRotated() {
-              if (this.num <= 0) {
-                  alert('已经木有次数了！');
-                  return false;
-              }
-              if (this.isLocking) {
-                  return false;
-              }
-              return true;
-            },
+        // 剩余抽奖次数
+        num: 2,
+      }
+    },
+    methods: {
+      startRotation() {
+        // 如果还不可以转动
+        if (!this.canBeRotated()) {
+          return false;
         }
+        // 开始转动
+        // 先上锁
+        this.isLocking = true;
+        // 设置在哪里停下，应该与后台交互，这里随机抽取0~5
+        const index = Math.floor(Math.random() * this.prizeData.length);
+        // 成功后次数减少一次
+        this.num--;
+        this.prizeIndex = index;
+        // 告诉子组件，开始转动了
+        this.$refs.roundTurntable.rotate(index);
+      },
+      // 已经转动完转盘触发的函数
+      endRotation() {
+        // 提示中奖
+        alert(`恭喜您获奖啦,您的奖品是：${this.prizeData[this.prizeIndex].prizeName}`);
+        // 解锁
+        this.isLocking = false;
+      },
+      // 判断是否可以转动
+      canBeRotated() {
+        if (this.num <= 0) {
+          alert('已经木有次数了！');
+          return false;
+        }
+        if (this.isLocking) {
+          return false;
+        }
+        return true;
+      },
     }
+  }
 </script>
 
 <style lang="scss">
@@ -128,6 +135,30 @@
       top: calc(50% - 200px);
       width: 400px;
       height: 400px;
+    }
+    .turntable-name {
+      /*background: pink;*/
+      position: absolute;
+      left: 10px;
+      top: 20px;
+      width: calc(100% - 20px);
+      font-size: 26px;
+      text-align: center;
+      color: #fff;
+    }
+
+    .turntable-img {
+      position: absolute;
+      /*要居中就要50% - 宽度 / 2*/
+      left: calc(50% - 80px / 2);
+      top: 60px;
+      width: 80px;
+      height: 80px;
+      img {
+        display: inline-block;
+        width: 100%;
+        height: 100%;
+      }
     }
 
     .pointer {
--- src/components/roundTurntable.vue
+++ src/components/roundTurntable.vue
@@ -6,10 +6,7 @@
       </canvas>
       <div class="prize-container">
         <div v-for="(item, index) in prizeData" class="item" :style="getRotateAngle(index)">
-          <div class="name">{{ item.prizeName }}</div>
-          <div class="img">
-            <img :src="item.prizeImg">
-          </div>
+          <slot name="item" :item="item"></slot>
         </div>
       </div>
     </div>
@@ -17,96 +14,96 @@
 </template>
 
 <script>
-    export default {
-        name: 'round-turntable',
-        mounted() {
-            this.init();
-        },
-        props: {
-            prizeData: {
-                required: true
-            },
-            rotateCircle: {
-                default: 6
-            },
-            turntableStyleOption: {
-                default: () => {
-                    return {
-                        // 背景色
-                        prizeBgColors: ['#AE3EFF', '#4D3FFF', '#FC262C', '#3A8BFF', '#EE7602', '#FE339F'],
-                        // 转盘的外边框颜色
-                        borderColor: '#199301',
-                    }
-                }
-            },
-            duringTime: {
-                default: 4.5
-            }
-        },
-        data() {
-            return {
-                // 开始转动的角度
-                startRotateDegree: 0,
-                rotateAngle: 0,
-                rotateTransition: '',
-            }
-        },
-        methods: {
-            // 根据index计算每一格要旋转的角度的样式
-            getRotateAngle(index) {
-                const angle = 360 / this.prizeData.length * index + (180 / this.prizeData.length);
-                return {
-                    transform: `rotate(${angle}deg)`
-                };
-            },
-            // 初始化圆形转盘canvas
-            init() {
-                // 各种数据
-                const data = this.turntableStyleOption;
-                const prizeNum = this.prizeData.length;
-                const {prizeBgColors, borderColor} = data;
-                // 开始绘画
-                const canvas = this.$refs.canvas;
-                const ctx = canvas.getContext("2d");
-                const canvasW = this.$refs.canvas.width = this.$refs.turntable.clientWidth; // 画板的高度
-                const canvasH = this.$refs.canvas.height = this.$refs.turntable.clientHeight; // 画板的宽度
-                // translate方法重新映射画布上的 (0,0) 位置
-                ctx.translate(0, canvasH);
-                // rotate方法旋转当前的绘图，因为文字适合当前扇形中心线垂直的！
-                ctx.rotate(-90 * Math.PI / 180);
-                // 圆环的外圆的半径
-                const outRadius = canvasW / 2;
-                // 圆环的内圆的半径
-                const innerRadius = 0;
-                const baseAngle = Math.PI * 2 / prizeNum; // 计算每个奖项所占角度数
-                ctx.clearRect(0, 0, canvasW, canvasH); //去掉背景默认的黑色
-                ctx.strokeStyle = borderColor; // 设置画图线的颜色
-                for (let index = 0; index < prizeNum; index++) {
-                    const angle = index * baseAngle;
-                    ctx.fillStyle = prizeBgColors[index]; //设置每个扇形区域的颜色
-                    ctx.beginPath(); //开始绘制
-                    // 标准圆弧：arc(x,y,radius,startAngle,endAngle,anticlockwise)
-                    ctx.arc(canvasW * 0.5, canvasH * 0.5, outRadius, angle, angle + baseAngle, false);
-                    ctx.arc(canvasW * 0.5, canvasH * 0.5, innerRadius, angle + baseAngle, angle, true);
-                    ctx.stroke();//开始链线
-                    ctx.fill(); //填充颜色
-                    ctx.save(); //保存当前环境的状态
-                }
-            },
-            // 转动起来
-            rotate(index) {
-                // 运转时长
-                const duringTime = this.duringTime;
-                const rotateAngle = this.startRotateDegree + this.rotateCircle * 360 + 360 - (180 / this.prizeData.length + 360 / this.prizeData.length * index) - this.startRotateDegree % 360;
-                this.startRotateDegree = rotateAngle;
-                this.rotateAngle = `rotate(${rotateAngle}deg)`;
-                this.rotateTransition = `transform ${duringTime}s cubic-bezier(0.250, 0.460, 0.455, 0.995)`;
-                setTimeout(() => {
-                    this.$emit('endRotation');
-                }, duringTime * 1000 + 500)
-            },
-        },
-    }
+  export default {
+    name: 'round-turntable',
+    mounted() {
+      this.init();
+    },
+    props: {
+      prizeData: {
+        required: true
+      },
+      rotateCircle: {
+        default: 6
+      },
+      turntableStyleOption: {
+        default: () => {
+          return {
+            // 背景色
+            prizeBgColors: ['#AE3EFF', '#4D3FFF', '#FC262C', '#3A8BFF', '#EE7602', '#FE339F'],
+            // 转盘的外边框颜色
+            borderColor: '#199301',
+          }
+        }
+      },
+      duringTime: {
+        default: 4.5
+      }
+    },
+    data() {
+      return {
+        // 开始转动的角度
+        startRotateDegree: 0,
+        rotateAngle: 0,
+        rotateTransition: '',
+      }
+    },
+    methods: {
+      // 根据index计算每一格要旋转的角度的样式
+      getRotateAngle(index) {
+        const angle = 360 / this.prizeData.length * index + (180 / this.prizeData.length);
+        return {
+          transform: `rotate(${angle}deg)`
+        };
+      },
+      // 初始化圆形转盘canvas
+      init() {
+        // 各种数据
+        const data = this.turntableStyleOption;
+        const prizeNum = this.prizeData.length;
+        const {prizeBgColors, borderColor} = data;
+        // 开始绘画
+        const canvas = this.$refs.canvas;
+        const ctx = canvas.getContext("2d");
+        const canvasW = this.$refs.canvas.width = this.$refs.turntable.clientWidth; // 画板的高度
+        const canvasH = this.$refs.canvas.height = this.$refs.turntable.clientHeight; // 画板的宽度
+        // translate方法重新映射画布上的 (0,0) 位置
+        ctx.translate(0, canvasH);
+        // rotate方法旋转当前的绘图，因为文字适合当前扇形中心线垂直的！
+        ctx.rotate(-90 * Math.PI / 180);
+        // 圆环的外圆的半径
+        const outRadius = canvasW / 2;
+        // 圆环的内圆的半径
+        const innerRadius = 0;
+        const baseAngle = Math.PI * 2 / prizeNum; // 计算每个奖项所占角度数
+        ctx.clearRect(0, 0, canvasW, canvasH); //去掉背景默认的黑色
+        ctx.strokeStyle = borderColor; // 设置画图线的颜色
+        for (let index = 0; index < prizeNum; index++) {
+          const angle = index * baseAngle;
+          ctx.fillStyle = prizeBgColors[index]; //设置每个扇形区域的颜色
+          ctx.beginPath(); //开始绘制
+          // 标准圆弧：arc(x,y,radius,startAngle,endAngle,anticlockwise)
+          ctx.arc(canvasW * 0.5, canvasH * 0.5, outRadius, angle, angle + baseAngle, false);
+          ctx.arc(canvasW * 0.5, canvasH * 0.5, innerRadius, angle + baseAngle, angle, true);
+          ctx.stroke();//开始链线
+          ctx.fill(); //填充颜色
+          ctx.save(); //保存当前环境的状态
+        }
+      },
+      // 转动起来
+      rotate(index) {
+        // 运转时长
+        const duringTime = this.duringTime;
+        const rotateAngle = this.startRotateDegree + this.rotateCircle * 360 + 360 - (180 / this.prizeData.length + 360 / this.prizeData.length * index) - this.startRotateDegree % 360;
+        this.startRotateDegree = rotateAngle;
+        this.rotateAngle = `rotate(${rotateAngle}deg)`;
+        this.rotateTransition = `transform ${duringTime}s cubic-bezier(0.250, 0.460, 0.455, 0.995)`;
+        setTimeout(() => {
+          this.$emit('endRotation');
+        }, duringTime * 1000 + 500)
+      },
+    },
+  }
 </script>
 
 <style scoped lang="scss">
@@ -117,7 +114,6 @@
     top: 0;
     text-align: center;
     transform: translateZ(0);
-
     .myTurntable {
       position: absolute;
       left: 0;
@@ -125,14 +121,12 @@
       width: 100%;
       height: 100%;
     }
-
     .prize-container {
       position: absolute;
       left: 25%;
       top: 0;
       width: 50%;
       height: 50%;
-
       .item {
         /*background: pink;*/
         position: absolute;
@@ -141,32 +135,6 @@
         width: 100%;
         height: 100%;
         transform-origin: center bottom;
-
-        .name {
-          /*background: pink;*/
-          position: absolute;
-          left: 10px;
-          top: 20px;
-          width: calc(100% - 20px);
-          font-size: 26px;
-          text-align: center;
-          color: #fff;
-        }
-
-        .img {
-          position: absolute;
-          /*要居中就要50% - 宽度 / 2*/
-          left: calc(50% - 80px / 2);
-          top: 60px;
-          width: 80px;
-          height: 80px;
-
-          img {
-            display: inline-block;
-            width: 100%;
-            height: 100%;
-          }
-        }
       }
     }
   }
